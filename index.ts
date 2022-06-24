3d1b2ab5b160235fe2f078a2e29a8541505e11dd
Gy <406631892@qq.com>
2022-06-24 17:17:13 +0800

    根据url返回不同的内容

--- index.ts
+++ index.ts
@@ -1,22 +1,37 @@
 import * as http from "http";
-
+import * as fs from "fs";
+import * as p from "path";
 const server = http.createServer();
 
+const publicPath = p.resolve(__dirname, "public");
+
 server.on("request", (req: http.IncomingMessage, res: http.ServerResponse) => {
-  console.log("req method", req.method);
-  console.log("req url", req.url);
-  console.log("req headers", req.headers);
-  const arr: any[] = [];
-  req.on("data", (chuck) => {
-    arr.push(chuck as any);
-  });
-  req.on("end", () => {
-    const body = Buffer.concat(arr).toString();
-    console.log("body", body);
-  });
-  res.statusCode = 404;
-  res.setHeader("X-f", "I am f");
-  res.end();
+  const { method, url, headers } = req;
+  console.log(url);
+  switch (url) {
+    case "/index.html":
+      fs.readFile(p.resolve(publicPath, "index.html"), (err, data) => {
+        if (err) throw err;
+        res.end(data.toString());
+      });
+      break;
+    case "/style.css":
+      res.setHeader("Content-Type", "text/css;charset=utf-8");
+      fs.readFile(p.resolve(publicPath, "style.css"), (err, data) => {
+        if (err) throw err;
+        res.end(data.toString());
+      });
+      break;
+    case "/main.js":
+      res.setHeader("Content-Type", "text/javascript;charset=utf-8");
+      fs.readFile(p.resolve(publicPath, "main.js"), (err, data) => {
+        if (err) throw err;
+        res.end(data.toString());
+      });
+      break;
+  }
 });
 
-server.listen(8888);
+server.listen(8888, () => {
+  console.log("success");
+});
