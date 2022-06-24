0737c7c0d6b95753d417ed5bd708331fa545059e
Gy <406631892@qq.com>
2022-06-24 15:56:03 +0800

    req 对象

--- index.ts
+++ index.ts
@@ -2,8 +2,18 @@ import * as http from "http";
 
 const server = http.createServer();
 
-server.on("request", (req, res) => {
-  console.log("有人请求了");
+server.on("request", (req: http.IncomingMessage, res: http.ServerResponse) => {
+  console.log("req method", req.method);
+  console.log("req url", req.url);
+  console.log("req headers", req.headers);
+  const arr: any[] = [];
+  req.on("data", (chuck) => {
+    arr.push(chuck as any);
+  });
+  req.on("end", () => {
+    const body = Buffer.concat(arr).toString();
+    console.log("body", body);
+  });
   res.end("hi");
 });
 
