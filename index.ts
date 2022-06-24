83962c96f5a3470605a36b77c1b8349f31d60065
Gy <406631892@qq.com>
2022-06-24 18:27:48 +0800

    可以处理任意文件 并且加上了404

--- index.ts
+++ index.ts
@@ -8,34 +8,33 @@ const publicPath = p.resolve(__dirname, "public");
 
 server.on("request", (req: http.IncomingMessage, res: http.ServerResponse) => {
   const { method, url: path, headers } = req;
-  const { search, pathname } = url.parse(path);
-  switch (pathname) {
-    case "/index.html":
-      fs.readFile(p.resolve(publicPath, "index.html"), (err, data) => {
-        if (err) throw err;
-        res.end(data.toString());
-      });
-      break;
-    case "/style.css":
-      res.setHeader("Content-Type", "text/css;charset=utf-8");
-      fs.readFile(p.resolve(publicPath, "style.css"), (err, data) => {
-        if (err) throw err;
-        res.end(data.toString());
-      });
-      break;
-    case "/main.js":
-      res.setHeader("Content-Type", "text/javascript;charset=utf-8");
-      fs.readFile(p.resolve(publicPath, "main.js"), (err, data) => {
-        if (err) throw err;
-        res.end(data.toString());
-      });
-      break;
-    default:
-      res.statusCode = 404;
-      res.end();
-  }
+  const { search, pathname } = url.parse(path as string);
+  let filename = pathname!.slice(1);
+  if (filename === "") filename = "index.html";
+  fs.readFile(p.resolve(publicPath, filename), (err, data) => {
+    if (err) {
+      if (err.errno === -2) {
+        res.statusCode = 404;
+        fs.readFile(p.resolve(publicPath, "notFound.html"), (err, data) => {
+          res.end(data);
+        });
+      } else {
+        res.statusCode = 500;
+        res.end("server error");
+      }
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(8888, () => {
   console.log("success");
 });
+
+// case "/index.html":
+//       fs.readFile(p.resolve(publicPath, "index.html"), (err, data) => {
+//         if (err) throw err;
+//         res.end(data.toString());
+//       });
+//       break;
